799ccce0c7c72e580d0282fdf70d39cd09670f25
azeng233 <1239571995@qq.com>
2021-08-06 10:24:27 +0800

    push

---
+++ mybatis-07/src/main/java/cn/zengchen233/dao/StudentMapper.java
@@ -0,0 +1,9 @@
+package cn.zengchen233.dao;
+
+import cn.zengchen233.pojo.Student;
+
+import java.util.List;
+
+public interface StudentMapper {
+
+}
---
+++ mybatis-07/src/main/java/cn/zengchen233/pojo/Teacher.java
@@ -0,0 +1,13 @@
+package cn.zengchen233.pojo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Teacher {
+    private int id;
+    private String name;
+    //一个老师拥有多个学生
+    private List<Student> students;
+}
