8835964af07bba7f4e03fc5e913d908583aa2f29
zengchen <1239571995@qq.com>
2023-02-02 12:57:38 +0800

    回炉重造mybatis

--- mybatis-06/src/main/java/cn/zengchen233/pojo/Teacher.java
+++
@@ -1,9 +0,0 @@
-package cn.zengchen233.pojo;
-
-import lombok.Data;
-
-@Data
-public class Teacher {
-    private int id;
-    private String name;
-}
--- mybatis-07/src/main/java/cn/zengchen233/dao/StudentMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.zengchen233.dao;
-
-import cn.zengchen233.pojo.Student;
-
-import java.util.List;
-
-public interface StudentMapper {
-
-}
